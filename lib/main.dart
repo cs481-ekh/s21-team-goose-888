d52170aadd9ef46c1cfe3fabedd6de3ebcaed0d3
Desmond <johnmartin689@u.boisestate.edu>
2021-02-16 11:08:28 -0700

    added UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,197 @@
 import 'package:flutter/material.dart';
+import 'UserInfo.dart';
+
 
 void main() {
-  runApp(MyApp());
+
+  //runApp(MyApp());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+     // '/': (context) => Rout2(),
+      '/': (context) => HomePage(),
+      '/second': (context) => Rout2(),
+
+
+    },
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // Try running your application with "flutter run". You'll see the
+//         // application has a blue toolbar. Then, without quitting the app, try
+//         // changing the primarySwatch below to Colors.green and then invoke
+//         // "hot reload" (press "r" in the console where you ran "flutter run",
+//         // or simply save your changes to "hot reload" in a Flutter IDE).
+//         // Notice that the counter didn't reset back to zero; the application
+//         // is not restarted.
+//         primarySwatch: Colors.blue,
+//         // This makes the visual density adapt to the platform that you run
+//         // the app on. For desktop platforms, the controls will be smaller and
+//         // closer together (more dense) than on mobile platforms.
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//       ),
+//      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+//       home: HomePage(),
+//
+//     );
+//   }
+//}
+
+
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+  //SecondRoute createState() => SecondRoute();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _formKey = GlobalKey<FormState>();
+  var isLoading = false;
+
+  void _submit() {
+    final isValid = _formKey.currentState.validate();
+    if (!isValid) {
+      return;
+    }
+    _formKey.currentState.save();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("ITD-888 Login"),
+        //leading: Icon(Icons.filter_vintage),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      //body
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        //padding: EdgeInsets.only(left: 50, right: 50),
+
+        //form
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+
+              //styling
+
+              TextFormField(
+                decoration: InputDecoration(labelText: 'E-Mail'),
+                keyboardType: TextInputType.emailAddress,
+                onFieldSubmitted: (value) {
+                  //Validator
+                },
+                validator: (value) {
+                  if (value.isEmpty ||
+                      !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value)) {
+                    return 'Enter a valid email!';
+                  }
+                  return null;
+                },
+              ),
+              //box styling
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.1,
+              ),
+              //text input
+              TextFormField(
+
+                decoration: InputDecoration(labelText: 'Password'),
+                keyboardType: TextInputType.emailAddress,
+                onFieldSubmitted: (value) {},
+                obscureText: true,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Enter a valid password!';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.08,
+              ),
+              RaisedButton(
+                padding: EdgeInsets.symmetric(
+                  vertical: 1.0,
+                  horizontal: 10.0,
+                ),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+                onPressed: () => _submit(),
+              ),
+             Row (
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+               children: <Widget>[
+
+                TextButton(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+
+                child: Text('Register for Account'),
+                ),
+                onPressed: () => _submit(),
+              ),
+              TextButton(
+                child: Align(
+                  alignment: Alignment.bottomLeft,
+
+                  child: Text('Forgot Password?'),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/second');
+                },
+              ),
+               ],
+              ),
+
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+
+  }
+
+}
+class Rout2 extends StatefulWidget {
+  @override
+  //_HomePageState createState() => _HomePageState();
+  SecondRoute createState() => SecondRoute();
+}
+class SecondRoute extends State<Rout2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Click Me Page"),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Back!'),
+        ),
+      ),
     );
   }
 }
