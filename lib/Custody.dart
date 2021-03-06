62788c9c17e4c82eddaabb89cf71b72a33c700b1
Desmond <johnmartin689@u.boisestate.edu>
2021-03-06 13:01:34 -0700

    got it going with a stateful Widget

--- lib/Custody.dart
+++ lib/Custody.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Custody extends StatelessWidget {
+class Custody extends StatefulWidget {
+  @override
+  _Custody createState() => _Custody();
+}
+
+class _Custody extends State<Custody> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,4 +24,23 @@ class Custody extends StatelessWidget {
     );
   }
 }
+//   class Custody extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Registration"),
+//         backgroundColor: Colors.blue,
+//       ),
+//       body: Center(
+//         child: RaisedButton(
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//           child: Text('Back!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
