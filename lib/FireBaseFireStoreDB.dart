522167f8bace75a00926f47cad11b11230812418
zach laptop <zacharygarner150@u.boisestate.edu>
2021-03-18 14:42:29 -0600

    Created Setter and Getter for main project reference ITD-88

--- lib/FireBaseFireStoreDB.dart
+++ lib/FireBaseFireStoreDB.dart
@@ -2,42 +2,100 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-abstract class fireStoreDb {
-  Future<String> serialNumber();
+abstract class FireStoreDb {
+  // current project will be the id of the selected project
+  //this id will be used to reference all other parts of the form.
+  var currentProject;
 
-  Future<String> status();
+  // First set of methods are methods to get info from the ITD-888 collection
+  String getSerialNumber();
+  String getStatus();
+  String getBidItem();
+  String getProjectName();
+  void selectProject(String serialNumber);
+  CollectionReference getProjects();
 
-  Future<String> bidItem();
+  // this method creates a new document in the ITD-888 collection with an
+  //auto generated id
+  DocumentReference createNewProject(String bidItem, String projectName, String remarks);
 
-  Future<String> projectName();
 }
 
-class db implements fireStoreDb {
+class StoreDb implements FireStoreDb {
   final FirebaseFirestore _firebaseFirestore =  FirebaseFirestore.instance;
+  var currentProject = 0;
 
   @override
-  Future<String> bidItem() {
-    // TODO: implement bidItem
-    throw UnimplementedError();
+  String getBidItem() {
+    if(currentProject == 0){
+      return "No Project Selected";
+    }else {
+      DocumentSnapshot snapshot = getProjects()
+          .doc(currentProject)
+          .get() as DocumentSnapshot;
+      Map<String, dynamic> data = snapshot.data();
+      return data['bidItem'];
+    }
   }
 
   @override
-  Future<String> projectName() {
-    // TODO: implement projectName
-    throw UnimplementedError();
+  String getProjectName() {
+      if(currentProject == 0){
+        return "No Project Selected";
+      }else {
+        DocumentSnapshot snapshot = getProjects()
+            .doc(currentProject)
+            .get() as DocumentSnapshot;
+        Map<String, dynamic> data = snapshot.data();
+        return data['projectName'];
+      }
   }
 
   @override
-  Future<String> serialNumber() {
-    // TODO: implement serialNumber
-    throw UnimplementedError();
+  String getSerialNumber() {
+    return currentProject;
   }
 
   @override
-  Future<String> status() {
-    // TODO: implement status
-    throw UnimplementedError();
+  String getStatus() {
+    if(currentProject == 0){
+      return "No Project Selected";
+    }else {
+      DocumentSnapshot snapshot = getProjects()
+          .doc(currentProject)
+          .get() as DocumentSnapshot;
+      Map<String, dynamic> data = snapshot.data();
+      return data['status'];
+    }
   }
 
+  @override
+  CollectionReference getProjects()  {
+    return _firebaseFirestore.collection('ITD-888');
+  }
+
+  @override
+  DocumentReference createNewProject(String bidItem, String projectName, String remarks) {
+
+    CollectionReference projects = getProjects();
+
+    // add the new project to the db
+    DocumentReference newProject = projects.add({
+      'bidItem': bidItem,
+      'projectName': projectName,
+      'remarks': remarks,
+      'sampleDate': DateTime.now().microsecondsSinceEpoch
+    }) as DocumentReference;
+
+    // set current project to the project that we just added.
+    currentProject = newProject.id;
+
+    return newProject;
+  }
+
+  @override
+  void selectProject(String serialNumber) {
+    currentProject = serialNumber;
+  }
 
 }
