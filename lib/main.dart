c98f8443b9642611368bd574dfad54cd985b6b68
Desmond <johnmartin689@u.boisestate.edu>
2021-04-08 16:44:54 -0600

    All working with it from the project page file

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,16 @@ import 'T166.dart';
 import 'T30.dart';
 import 'T312.dart';
 import 'auth.dart';
-//import 'ProjectsPage.dart';
+import 'ProjectsPage.dart';
 import 'FireBaseFireStoreDB.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 String textEmail = "";
 //StoreDb db = StoreDb();
-StoreDb dbe = StoreDb();
+
 void main() {
+  StoreDb dbe = StoreDb();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: '/',
@@ -35,7 +36,9 @@ void main() {
       '/form': (context) => FormSections(
         db: dbe,
       ),
-      '/project': (context) => ProjectsPage(),
+      '/project': (context) => ProjectsPage(
+        db: dbe,
+      ),
       '/LooseMixR97': (context) => LooseMixR97(
         db: dbe,
       ),
@@ -232,196 +235,196 @@ class _HomeLogin extends State<HomeLogin> {
     );
   }
 }
-class ProjectsPage extends StatefulWidget {
-  @override
-  _ProjectsPage createState() => _ProjectsPage();
-}
-
-class _ProjectsPage extends State<ProjectsPage> {
-  var formKey = GlobalKey<FormState>();
-  var formKey2 = GlobalKey<FormState>();
-  //StoreDb db = StoreDb();
-  TextEditingController projectName = TextEditingController();
-  TextEditingController bidItem = TextEditingController();
-  void dispose() {
-    projectName.dispose();
-    bidItem.dispose();
-
-    super.dispose();
-  }
-  void initState() {
-    dbe.listProjects();
-    super.initState();
-  }
-  void createProject() {
-    dbe.createNewProject(projectName.text, bidItem.text);
-  }
-
-  var isLoading = false;
-  //String textEmail = "";
-
-  bool _submit() {
-    final isValid = formKey.currentState.validate();
-    if (!isValid) {
-      return false;
-    } else {
-      formKey.currentState.save();
-      return true;
-    }
-  }
-
-  // Future List<String> list_items;
-  String _dropDownValue = "Select Project";
-  String value = "";
-
-
-  Future<List> fetchData() async {
-    return dbe.listProjects();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Projects Available"),
-        backgroundColor: Colors.blue,
-      ),
-      body: SingleChildScrollView(
-
-
-        //padding: EdgeInsets.only(left: 50, right: 50),
-
-        //form
-        child: Column(children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            //  Padding(
-            //padding: const EdgeInsets.all(10.0),
-            child: Form(
-              key: formKey,
-              child: Column(
-                children: <Widget>[
-                  FutureBuilder<List<String>>(
-                    future: dbe.listProjects(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return DropdownButton(
-                          isExpanded: true,
-                          value: _dropDownValue,
-                          items: snapshot.data.map((value) {
-                            return new DropdownMenuItem(
-                              value: value,
-                              child: new Text(value),
-                            );
-                          }).toList(),
-                          onChanged: (newValue) {
-                            setState(() {
-                              _dropDownValue = newValue;
-                            });
-                          },
-                        );
-                      }
-                      return Center(child: CircularProgressIndicator());
-                    },
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.width * 0.05,
-                  ),
-                  RaisedButton(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 1.0,
-                      horizontal: 10.0,
-                    ),
-                    child: Text(
-                      "Go to Project",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    onPressed: () {
-                      //bool check = ;
-                      if (_submit()) {
-                        dbe.selectProject(_dropDownValue);
-                        Navigator.pushNamed(context, '/form');
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.width * 0.05,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            //  Padding(
-            //padding: const EdgeInsets.all(10.0),
-            child: Form(
-              key: formKey2,
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    controller: projectName,
-                    decoration: InputDecoration(labelText: "Project Name"),
-                    keyboardType: TextInputType.text,
-                    onFieldSubmitted: (value) {
-
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Enter a valid project name!';
-                      }
-                      //textEmail=value;
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: bidItem,
-                    decoration: InputDecoration(labelText: "Bid Item"),
-                    keyboardType: TextInputType.text,
-                    onFieldSubmitted: (value) {
-
-                    },
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return 'Enter a valid bid item!';
-                      }
-                      //textEmail=value;
-                      return null;
-                    },
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.width * 0.05,
-                  ),
-                  RaisedButton(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 1.0,
-                      horizontal: 10.0,
-                    ),
-                    child: Text(
-                      "Create Project",
-                      style: TextStyle(
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    onPressed: () {
-                      //bool check = ;
-                      if (_submit()) {
-                        createProject();
-                        Navigator.pushNamed(context, '/form');
-                      }
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ]
-        ),
-      ),
-    );
-
-  }
-}
+// class ProjectsPage extends StatefulWidget {
+//   @override
+//   _ProjectsPage createState() => _ProjectsPage();
+// }
+//
+// class _ProjectsPage extends State<ProjectsPage> {
+//   var formKey = GlobalKey<FormState>();
+//   var formKey2 = GlobalKey<FormState>();
+//   //StoreDb db = StoreDb();
+//   TextEditingController projectName = TextEditingController();
+//   TextEditingController bidItem = TextEditingController();
+//   void dispose() {
+//     projectName.dispose();
+//     bidItem.dispose();
+//
+//     super.dispose();
+//   }
+//   void initState() {
+//     dbe.listProjects();
+//     super.initState();
+//   }
+//   void createProject() {
+//     dbe.createNewProject(projectName.text, bidItem.text);
+//   }
+//
+//   var isLoading = false;
+//   //String textEmail = "";
+//
+//   bool _submit() {
+//     final isValid = formKey.currentState.validate();
+//     if (!isValid) {
+//       return false;
+//     } else {
+//       formKey.currentState.save();
+//       return true;
+//     }
+//   }
+//
+//   // Future List<String> list_items;
+//   String _dropDownValue = "Select Project";
+//   String value = "";
+//
+//
+//   Future<List> fetchData() async {
+//     return dbe.listProjects();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Projects Available"),
+//         backgroundColor: Colors.blue,
+//       ),
+//       body: SingleChildScrollView(
+//
+//
+//         //padding: EdgeInsets.only(left: 50, right: 50),
+//
+//         //form
+//         child: Column(children: <Widget>[
+//           Padding(
+//             padding: const EdgeInsets.all(10.0),
+//             //  Padding(
+//             //padding: const EdgeInsets.all(10.0),
+//             child: Form(
+//               key: formKey,
+//               child: Column(
+//                 children: <Widget>[
+//                   FutureBuilder<List<String>>(
+//                     future: dbe.listProjects(),
+//                     builder: (context, snapshot) {
+//                       if (snapshot.hasData) {
+//                         return DropdownButton(
+//                           isExpanded: true,
+//                           value: _dropDownValue,
+//                           items: snapshot.data.map((value) {
+//                             return new DropdownMenuItem(
+//                               value: value,
+//                               child: new Text(value),
+//                             );
+//                           }).toList(),
+//                           onChanged: (newValue) {
+//                             setState(() {
+//                               _dropDownValue = newValue;
+//                             });
+//                           },
+//                         );
+//                       }
+//                       return Center(child: CircularProgressIndicator());
+//                     },
+//                   ),
+//                   SizedBox(
+//                     height: MediaQuery.of(context).size.width * 0.05,
+//                   ),
+//                   RaisedButton(
+//                     padding: EdgeInsets.symmetric(
+//                       vertical: 1.0,
+//                       horizontal: 10.0,
+//                     ),
+//                     child: Text(
+//                       "Go to Project",
+//                       style: TextStyle(
+//                         fontSize: 16.0,
+//                       ),
+//                     ),
+//                     onPressed: () {
+//                       //bool check = ;
+//                       if (_submit()) {
+//                         dbe.selectProject(_dropDownValue);
+//                         Navigator.pushNamed(context, '/form');
+//                       }
+//                     },
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//           SizedBox(
+//             height: MediaQuery.of(context).size.width * 0.05,
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(10.0),
+//             //  Padding(
+//             //padding: const EdgeInsets.all(10.0),
+//             child: Form(
+//               key: formKey2,
+//               child: Column(
+//                 children: <Widget>[
+//                   TextFormField(
+//                     controller: projectName,
+//                     decoration: InputDecoration(labelText: "Project Name"),
+//                     keyboardType: TextInputType.text,
+//                     onFieldSubmitted: (value) {
+//
+//                     },
+//                     validator: (value) {
+//                       if (value.isEmpty) {
+//                         return 'Enter a valid project name!';
+//                       }
+//                       //textEmail=value;
+//                       return null;
+//                     },
+//                   ),
+//                   TextFormField(
+//                     controller: bidItem,
+//                     decoration: InputDecoration(labelText: "Bid Item"),
+//                     keyboardType: TextInputType.text,
+//                     onFieldSubmitted: (value) {
+//
+//                     },
+//                     validator: (value) {
+//                       if (value.isEmpty) {
+//                         return 'Enter a valid bid item!';
+//                       }
+//                       //textEmail=value;
+//                       return null;
+//                     },
+//                   ),
+//                   SizedBox(
+//                     height: MediaQuery.of(context).size.width * 0.05,
+//                   ),
+//                   RaisedButton(
+//                     padding: EdgeInsets.symmetric(
+//                       vertical: 1.0,
+//                       horizontal: 10.0,
+//                     ),
+//                     child: Text(
+//                       "Create Project",
+//                       style: TextStyle(
+//                         fontSize: 16.0,
+//                       ),
+//                     ),
+//                     onPressed: () {
+//                       //bool check = ;
+//                       if (_submit()) {
+//                         createProject();
+//                         Navigator.pushNamed(context, '/form');
+//                       }
+//                     },
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ]
+//         ),
+//       ),
+//     );
+//
+//   }
+// }
 
