d670407fc10187dc42dbbec1c404d25ebfa0c1dc
zach laptop <zacharygarner150@u.boisestate.edu>
2021-04-07 18:52:22 -0600

    added more setters and getters for db

--- lib/FireBaseFireStoreDB.dart
+++ lib/FireBaseFireStoreDB.dart
@@ -46,6 +46,18 @@ class StoreDb implements FireStoreDb {
   Map<String, dynamic> t312;
   Map<String, dynamic> t329;
 
+  StoreDb(){
+    custody = {};
+    r47 = {};
+    r97 = {};
+    t166 = {};
+    t209 = {};
+    t30 = {};
+    t308 = {};
+    t312 = {};
+    t329 = {};
+  }
+
   @override
   Future<String> getBidItem() async {
     if (currentProject == 0) {
